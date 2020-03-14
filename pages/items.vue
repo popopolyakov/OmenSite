d23e012e796840b89bd91c3237fbd4d4dc5de88a
CUDDLER <bagldya@gmail.com>
2020-03-14 05:41:27 +0300

    заглушка айтемс

--- pages/items.vue
+++ pages/items.vue
@@ -1,24 +1,34 @@
 <template lang="pug">
   .container
     navigation
-    .page
-      links
+    .items__page
+      omenlogo
+      div.text
+        | Страница в разработке
 </template>
 
 <script>
 import links from '~/components/links.vue'
 import navigation from '~/components/navigation.vue'
 import trans from '~/mixins/trans'
+import omenlogo from '~/components/omenlogo.vue'
 export default {
   mixins: [trans],
   components: {
-    navigation, links
+    navigation, omenlogo
   }
 }
 </script>
 
 <style lang="sass">
 
-.container
-
+.items__page
+  display: flex
+  justify-content: center
+  align-items: center
+  height: 100vh
+  flex-direction: column
+  .text
+    font-family: Open Sans Condensed
+    font-size: 25pt
 </style>
